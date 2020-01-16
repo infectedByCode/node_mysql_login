8c7f7e9349546b0a301b25dc33d0131b2986c9e9
Matt Daley <mdaleyhk@gmail.com>
2020-01-16 14:42:13 +0000

    Connect mysql DB

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const session = require('express-session');
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'pineappleGold',
+  database: 'mysql_test'
+});
+
+connection.connect();
 
 const app = express();
 
