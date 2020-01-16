326fe5bb6d960b737bfb87b0a40bb901199bc876
Matt Daley <mdaleyhk@gmail.com>
2020-01-16 16:56:17 +0000

    Include tests for errors on POST /api/auth loginUser

--- spec/app.spec.js
+++ spec/app.spec.js
@@ -1,17 +1,42 @@
 const { expect } = require('chai');
 const request = require('supertest');
 const app = require('../app');
-// const connection = require('../db/connection');
+const connection = require('../db/connection');
 
 describe('/api', () => {
+  after(() => {
+    connection.end();
+  });
   describe('/auth', () => {
-    it('GET:200, returns a user object if username and password are found', () => {
+    it('POST:200, returns a user object if username and password are found', () => {
       return request(app)
-        .get('/api/auth')
+        .post('/api/auth')
+        .send({ username: 'user1288', password: 'password' })
         .expect(200)
         .then(({ body: { user } }) => {
           expect(user).to.be.an('object');
+          expect(user).to.have.keys(['uuid', 'username', 'user_password', 'email']);
         });
+    });
+    describe('ERRORS /api/auth', () => {
+      it("POST:400, when user's username or password are missing", () => {
+        return request(app)
+          .post('/api/auth')
+          .send({ username: 'user1288' })
+          .expect(400)
+          .then(({ body: { msg } }) => {
+            expect(msg).to.equal('Please enter your username and password.');
+          });
+      });
+      it("POST:404, when user's details are not found", () => {
+        return request(app)
+          .post('/api/auth')
+          .send({ username: 'user12', password: 'password' })
+          .expect(404)
+          .then(({ body: { msg } }) => {
+            expect(msg).to.equal('Username or password are not correct.');
+          });
+      });
     });
   });
 });
