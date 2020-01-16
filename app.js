f36f8bcf334077e2f613a05f2c19d3e1e9906d32
Matt Daley <mdaleyhk@gmail.com>
2020-01-16 17:59:26 +0000

    Add cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const session = require('express-session');
+const cors = require('cors');
 
 const apiRouter = require('./routes/api-router');
 
 const app = express();
+app.use(cors());
 app.use(
   session({
     secret: 'secret',
