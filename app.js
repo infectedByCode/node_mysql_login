ee76ae84cfe815809e37d44e7209566c13f9beeb
Matt Daley <mdaleyhk@gmail.com>
2020-01-17 11:34:20 +0000

    Refactor code to use env variables

--- app.js
+++ app.js
@@ -1,19 +1,26 @@
 const express = require('express');
 const session = require('express-session');
 const cors = require('cors');
+require('dotenv').config();
 
 const { checkAuth } = require('./auth/auth');
 const apiRouter = require('./routes/api-router');
 
 const app = express();
+
+const sessionConfig = {
+  secret: process.env.COOKIESECRET,
+  resave: true,
+  saveUninitialized: true,
+  cookie: {
+    domain: process.env.COOKIESECRET,
+    maxAge: 3600000,
+    secure: process.env === 'production'
+  }
+};
+
 app.use(cors());
-app.use(
-  session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-  })
-);
+app.use(session(sessionConfig));
 app.use(express.json());
 
 app.get('/', checkAuth, (req, res, next) => {
--- db/connection.js
+++ db/connection.js
@@ -1,10 +1,10 @@
 const mysql = require('mysql');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'pineappleGold',
-  database: 'login_users'
+  host: process.env.DOMAIN,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
 });
 
 module.exports = connection;
