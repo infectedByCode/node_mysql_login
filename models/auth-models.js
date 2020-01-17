214265f749bd5573a7d85548b691326538eb3c8e
Matt Daley <mdaleyhk@gmail.com>
2020-01-17 11:13:13 +0000

    Remove returning of password from signinUser.

--- models/auth-models.js
+++ models/auth-models.js
@@ -3,7 +3,7 @@ const connection = require('../db/connection');
 exports.signinUser = (username, password, cb) => {
   if (username && password) {
     connection.query(
-      'SELECT * FROM users WHERE username = ? AND user_password = ?;',
+      'SELECT username, email, uuid FROM users WHERE username = ? AND user_password = ?;',
       [username, password],
       (error, results) => {
         if (error) return cb(error);
